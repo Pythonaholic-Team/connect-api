b6b97106242a41b8312bf61c6df0256d4a274fd3
adhammhaydat <adhammohidat123@gmail.com>
2021-12-12 11:41:57 +0200

    feat: create accpunts app to handle registeration users

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import RegisterView
+#  LogoutAPIView, LoginAPIView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name="register"),
+
+
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+# from rest_framework import generics, status, views, permissions
+from .serializers import RegisterSerializer
+# from rest_framework.response import Response
+# from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+# from rest_framework.views import APIView
+import jwt
+# from django.conf import settings
 
-# Create your views here.
+from django.http import HttpResponsePermanentRedirect
+import os
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+
+class CustomRedirect(HttpResponsePermanentRedirect):
+
+    allowed_schemes = [os.environ.get('APP_SCHEME'), 'http', 'https']
+
+
+class RegisterView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+
